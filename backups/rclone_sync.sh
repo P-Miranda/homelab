4101ba480f429979254cffa251388efa9e4fec29
P-Miranda <pedro.r.miranda@tecnico.ulisboa.pt>
2024-04-05 23:09:05 +0100

    fix(rclone): use .env variables - update `rclone_sync.sh` script to use environment variables

--- backups/rclone_sync.sh
+++ backups/rclone_sync.sh
@@ -4,19 +4,15 @@
 #
 
 SCRIPT_PATH=$(dirname -- "$0")
-RCLONE_LOG=${SCRIPT_PATH}/rclone.log
+ENV_FILE=$SCRIPT_PATH/.env
 
-# Sync Restic backup
-LOCAL_BACKUP_DIR=/media/NAS-home/restic-backup
-B2_REMOTE=Restic-B2-Crypt:
-
-# RClone Flags:
-# --progress: show progress
-# --checksum: filename, filesize and SHA-1 checksum comparison
-# --fast-list: more efficient API calls
-# --transfers=32: up to 32 parallel transfers ~90MB RAM per transfer
-# --bwlimit UPLOAD:DOWNLOAD limit upload and download speeds
-RCLONE_FLAGS='--progress --fast-list --transfers=32 --bwlimit=75M'
+if [ -f $ENV_FILE ]
+then
+  source $ENV_FILE
+else
+  echo "No .env file found."
+  exit 1
+fi
 
 SUCCESS=1
 check_command() {
@@ -27,11 +23,20 @@ check_command() {
     fi
 }
 
+# RClone Flags:
+# --progress: show progress
+# --checksum: filename, filesize and SHA-1 checksum comparison
+# --fast-list: more efficient API calls
+# --transfers=32: up to 32 parallel transfers ~90MB RAM per transfer
+# --bwlimit UPLOAD:DOWNLOAD limit upload and download speeds
+RCLONE_FLAGS='--progress --fast-list --transfers=32 --bwlimit=75M'
+
 # RClone Sync command
-rclone $RCLONE_FLAGS sync $LOCAL_BACKUP_DIR $B2_REMOTE > $RCLONE_LOG 2>&1
+rclone $RCLONE_FLAGS sync $RESTIC_REPO_PATH $RCLONE_REMOTE > $RCLONE_LOG 2>&1
 check_command
+
 echo "Restic Offsite Backup complete with size:" >> $RCLONE_LOG 2>&1
-rclone size $B2_REMOTE >> $RCLONE_LOG 2>&1
+rclone size $RCLONE_REMOTE >> $RCLONE_LOG 2>&1
 check_command
 
 #
